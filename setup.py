75cb3351cb81d45e44b4830838ecd5e804458544
Jeremy Lainé <jeremy.laine@m4x.org>
2018-08-30 14:16:42 +0200

    v0.7.5

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext as _build_ext
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+readme_file = os.path.join(root_dir, 'README.rst')
+with open(readme_file, encoding='utf-8') as f:
+    long_description = f.read()
+
 
 class build_ext(_build_ext):
     def finalize_options(self):
@@ -11,25 +19,12 @@ class build_ext(_build_ext):
         import numpy
         self.include_dirs.append(numpy.get_include())
 
-extensions = [
-    Extension('pysupercluster',
-        extra_compile_args=['-std=c++1y'],
-        language='c++',
-        depends=[
-            'src/kdbush.hpp',
-            'src/supercluster.hpp',
-        ],
-        sources=[
-            'src/module.cpp',
-            'src/supercluster.cpp',
-        ],
-    )
-]
 
 setup(
     name='pysupercluster',
-    version='0.7.4',
+    version='0.7.5',
     description='A fast geospatial point clustering module.',
+    long_description=long_description,
     author='Jeremy Lainé',
     author_email='jeremy@getwemap.com',
     url='https://github.com/wemap/pysupercluster',
@@ -46,5 +41,19 @@ setup(
     install_requires=['numpy'],
     python_requires='>=3',
     setup_requires=['numpy'],
-    ext_modules=extensions,
+    ext_modules=[
+        Extension(
+            'pysupercluster',
+            extra_compile_args=['-std=c++1y'],
+            language='c++',
+            depends=[
+                'src/kdbush.hpp',
+                'src/supercluster.hpp',
+            ],
+            sources=[
+                'src/module.cpp',
+                'src/supercluster.cpp',
+            ],
+        )
+    ]
 )
