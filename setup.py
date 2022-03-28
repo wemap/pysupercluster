7fb51a74522b000f05603b6c3cdae00634f843e7
Jeremy Lainé <jeremy.laine@m4x.org>
2022-03-29 00:01:22 +0200

    v0.7.7

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class build_ext(_build_ext):
 
 setup(
     name="pysupercluster",
-    version="0.7.6",
+    version="0.7.7",
     description="A fast geospatial point clustering module.",
     long_description=long_description,
     author="Jeremy Lainé",
