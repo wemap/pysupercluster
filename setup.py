ef4960da4bd38529cd2d6a870626312be830e3e8
Aleksey Semeshko <dromoc@mail.ru>
2023-04-19 09:03:32 +0300

    v0.7.8

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class build_ext(_build_ext):
 
 setup(
     name="pysupercluster",
-    version="0.7.7",
+    version="0.7.8",
     description="A fast geospatial point clustering module.",
     long_description=long_description,
     author="Jeremy Lainé",
